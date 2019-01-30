1d29602c7c32b89b6cfa4af5ec826807de2d47f6
Lucas Rodrigues <l.rsouza@outlook.com>
2019-01-30 07:58:26 -0200

    unit test

--- src/redux-flow/actions/login-action.test.js
+++ src/redux-flow/actions/login-action.test.js
@@ -1,13 +1,26 @@
 import {
   isDevelopmentEnvironment,
+  isValidUrlHandle,
 } from './login-actions';
-
 
 test('is development environment', () => {
   const window = {
-    location: "localhost:5000"
+    location: 'http://localhost:5000',
   };
 
   const url = isDevelopmentEnvironment();
   expect(url).toContain('development=true');
+});
+
+test('is valid url', () => {
+  const win = {
+    document: {
+      location: {
+        origin: 'http://localhost',
+      },
+    },
+  };
+
+  const isValid = isValidUrlHandle(win);
+  expect(isValid).toBeTruthy();
 });
--- src/redux-flow/actions/login-actions.js
+++ src/redux-flow/actions/login-actions.js
@@ -1,57 +1,50 @@
+import Axios from 'axios';
+
 import {
-    USER_AUTHENTICATION,
-    ADD_CREDENTIALS,
-    LOGIN_TOKEN
-} from "../../constants";
-import Axios from "axios";
-
-
-export const submitCredentials = (credentials, history) => {
-    return dispatch => {
-        return Axios.get(isDevelopmentEnvironment()).then(response => {
-
-            let win = window.open(response.data, '', 'location=no,toolbar=0');
-
-            let polTimer = window.setInterval(() => {
-                let isValidUrl = isValidUrlHandle(win)
-
-                if (isValidUrl) {
-                    clearInterval(polTimer);
-                    var access_token = win.document.location.pathname.split('=')[1];
-                    win.close();
-                    window.sessionStorage.setItem('access_token', access_token);
-
-                    history.push('/main');
-
-                    dispatch({
-                        type: USER_AUTHENTICATION,
-                        payload: access_token
-                    });
-                }
-            }, 600)
-        });
-    }
-}
-
-
+  USER_AUTHENTICATION,
+  ADD_CREDENTIALS,
+  LOGIN_TOKEN,
+} from '../../constants';
 
 export const isDevelopmentEnvironment = () => {
-    if(window.location.origin.indexOf("localhost") >= 0)
-        return LOGIN_TOKEN+"?development=true";
-    else
-        return LOGIN_TOKEN;
-}
+  if (window.location.origin.indexOf('localhost') >= 0) {
+    return `${LOGIN_TOKEN}?development=true`;
+  }
+  return LOGIN_TOKEN;
+};
 
 export const isValidUrlHandle = (win) => {
-    try {
-        return win.document.location.origin === window.location.origin
-    } catch (error) {
-        return false
-    }
-}
+  try {
+    return win.document.location.origin === window.document.location.origin;
+  } catch (error) {
+    return false;
+  }
+};
 
+export const submitCredentials = (credentials, history) => dispatch => Axios
+  .get(isDevelopmentEnvironment()).then((response) => {
+    const win = window.open(response.data, '', 'location=no,toolbar=0');
 
-export const addCredentials = (credentials) => ({
-    type: ADD_CREDENTIALS,
-    payload: credentials
-})
+    const polTimer = window.setInterval(() => {
+      const isValidUrl = isValidUrlHandle(win);
+
+      if (isValidUrl) {
+        clearInterval(polTimer);
+        const accessToken = win.document.location.pathname.split('=')[1];
+        win.close();
+        window.sessionStorage.setItem('access_token', accessToken);
+
+        history.push('/main');
+
+        dispatch({
+          type: USER_AUTHENTICATION,
+          payload: accessToken,
+        });
+      }
+    }, 600);
+  });
+
+export const addCredentials = credentials => ({
+  type: ADD_CREDENTIALS,
+  payload: credentials,
+});
