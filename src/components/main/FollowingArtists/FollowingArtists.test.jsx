9a7fdb44a4661a0b76f8cfbcb531da6bb755da5b
Lucas Rodrigues <l.rsouza@outlook.com>
2019-02-04 07:56:20 -0200

    added following artists unit tests

--- src/components/main/FollowingArtists/FollowingArtists.test.jsx
+++ src/components/main/FollowingArtists/FollowingArtists.test.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import FollowingArtists from './FollowingArtists';
 
+import { followingArtists } from '../UserInfo/__mocks__/objectsMocks';
+
 test('FollowingArtists snaphot test', () => {
   const component = renderer.create(<FollowingArtists />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+
+test('FollowingArtists list of card', () => {
+  const component = renderer.create(<FollowingArtists followingArtists={followingArtists} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
