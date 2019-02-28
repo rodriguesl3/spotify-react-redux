76106778167bc34e228674182022f39f391ee9f9
Lucas Rodrigues <l.rsouza@outlook.com>
2019-02-28 07:15:30 -0300

    grouped main actions unit test and added new unit test.

--- src/redux-flow/actions/main-actions.test.js
+++ src/redux-flow/actions/main-actions.test.js
@@ -30,66 +30,89 @@ const mockStore = configureStore(middlewares);
 
 jest.mock('../../constants/api');
 
-test('getting user info', (done) => {
-  get(selfInformation).then((response) => {
-    expect(response).toHaveProperty('data.email', 'lusk.rs@hotmail.com');
-    expect(response).toHaveProperty('status', 200);
+describe('User Info', () => {
+  test('getting user info', (done) => {
+    get(selfInformation).then((response) => {
+      expect(response).toHaveProperty('data.email', 'lusk.rs@hotmail.com');
+      expect(response).toHaveProperty('status', 200);
+    });
+    done();
   });
-  done();
-});
 
-test('getting whole function', () => {
-  const history = {
-    push: jest.fn(),
-  };
+  test('getting whole function', () => {
+    const history = {
+      push: jest.fn(),
+    };
 
-  const result = getUserInfo(history);
-  expect(result).toBeInstanceOf(Object);
-});
-
-test('getting error user info', (done) => {
-  get(null).then((response) => {
-    expect(response).toHaveProperty('status', 401);
+    const result = getUserInfo(history);
+    expect(result).toBeInstanceOf(Object);
   });
-  done();
+
+  test('getting error user info', (done) => {
+    get(null).then((response) => {
+      expect(response).toHaveProperty('status', 401);
+    });
+    done();
+  });
+
+  test('validating response redirect', () => {
+    const store = mockStore({});
+    const history = {};
+
+    userInfoResponse(userInfo, store.dispatch, history);
+    const actions = store.getActions();
+    expect(actions[0]).toHaveProperty('type', 'USER_INFORMATION');
+    expect(actions[0]).toHaveProperty('payload');
+  });
+
+  test('validating error response redirect', () => {
+    const store = mockStore({});
+    const history = {
+      push: jest.fn(),
+    };
+    userInfo.status = 401;
+
+    userInfoResponse(userInfo, store.dispatch, history);
+    const actions = store.getActions();
+    expect(actions[0]).toBeUndefined();
+    expect(history.push).toBeCalledWith('/login');
+  });
 });
 
-test('validating response redirect', () => {
-  const store = mockStore({});
-  const history = {};
+describe('User Listen Now', () => {
+  test('getting user listen now', () => {
+    const store = mockStore({});
+    dispatchUserListNow(userListening, store.dispatch);
+    const actions = store.getActions();
+    expect(actions[0]).toHaveProperty('type', 'USER_LISTENING');
+    expect(actions[0].payload).toEqual(userListening.data);
+  });
 
-  userInfoResponse(userInfo, store.dispatch, history);
-  const actions = store.getActions();
-  expect(actions[0]).toHaveProperty('type', 'USER_INFORMATION');
-  expect(actions[0]).toHaveProperty('payload');
+  test('not getting user listen now', () => {
+    const store = mockStore({});
+    userListening.status = 401;
+    dispatchUserListNow(userListening, store.dispatch);
+    const actions = store.getActions();
+    expect(actions[0]).toBeUndefined();
+    expect(actions.length).toEqual(0);
+  });
 });
 
-test('validating error response redirect', () => {
-  const store = mockStore({});
-  const history = {
-    push: jest.fn(),
-  };
-  userInfo.status = 401;
+describe('Following artists', () => {
+  test('getting following artists', () => {
+    const store = mockStore({});
+    dispatchFollowinArtists(followingArtists, store.dispatch);
+    const actions = store.getActions();
+    expect(actions[0]).toHaveProperty('type', 'ARTIST_FOLLOWED');
+    expect(actions[0].payload).toEqual(followingArtists.data);
+  });
 
-  userInfoResponse(userInfo, store.dispatch, history);
-  const actions = store.getActions();
-  expect(actions[0]).toBeUndefined();
-  expect(history.push).toBeCalledWith('/login');
-});
+  test('not getting following artists', () => {
+    const store = mockStore({});
+    followingArtists.status = 401;
 
-
-test('getting user listen now', () => {
-  const store = mockStore({});
-  dispatchUserListNow(userListening, store.dispatch);
-  const actions = store.getActions();
-  expect(actions[0]).toHaveProperty('type', 'USER_LISTENING');
-  expect(actions[0].payload).toEqual(userListening.data);
-});
-
-test('getting following artists', () => {
-  const store = mockStore({});
-  dispatchFollowinArtists(followingArtists, store.dispatch);
-  const actions = store.getActions();
-  expect(actions[0]).toHaveProperty('type', 'ARTIST_FOLLOWED');
-  expect(actions[0].payload).toEqual(followingArtists.data);
+    dispatchFollowinArtists(followingArtists, store.dispatch);
+    const actions = store.getActions();
+    expect(actions[0]).toBeUndefined();
+  });
 });
