a191ce8b04988606e4630b6bbbcb7dd955ab15cc
Lucas Rodrigues <l.rsouza@outlook.com>
2019-03-04 20:47:56 -0300

    added unit test to navbar actions

--- src/redux-flow/actions/navbar-actions.js
+++ src/redux-flow/actions/navbar-actions.js
@@ -2,23 +2,32 @@ import {
   SHOW_SEARCH,
   spotifySearch,
   SPOTIFY_SEARCH,
+  SPOTIFY_SEARCH_ERROR,
 } from '../../constants';
 
 import {
   get,
 } from '../../constants/api';
 
+
+export const validateSearchTrackArtists = (response, dispatch) => {
+  console.log(response);
+  if (response.status === 200) {
+    dispatch({
+      type: SPOTIFY_SEARCH,
+      payload: response.data,
+    });
+  } else {
+    dispatch({
+      type: SPOTIFY_SEARCH_ERROR,
+      payload: '',
+    });
+  }
+};
+
 export const searchingTracksArtists = query => (dispatch) => {
   const url = `${spotifySearch}?q=${query}&type=track%2Cartist&limit=10`;
-  get(url).then((response) => {
-    console.log(response);
-    if (response.status === 200) {
-      dispatch({
-        type: SPOTIFY_SEARCH,
-        payload: response.data,
-      });
-    }
-  });
+  get(url).then(response => validateSearchTrackArtists(response, dispatch));
 };
 
 export const isSearching = isVisible => (dispatch) => {
---
+++ src/redux-flow/actions/navbar-actions.test.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-undef */
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import {
+  isSearching,
+  validateSearchTrackArtists,
+} from './navbar-actions';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+test('is Searching test with true parameter', () => {
+  const store = mockStore({});
+  const isSearchResult = isSearching(true)(store.dispatch);
+
+  const actions = store.getActions();
+  console.log(actions[0]);
+  expect(actions[0]).toHaveProperty('type', 'SHOW_SEARCH');
+  expect(actions[0]).toHaveProperty('payload', true);
+});
+
+test('is Searching test with false parameter', () => {
+  const store = mockStore({});
+  isSearching(false)(store.dispatch);
+
+  const actions = store.getActions();
+  expect(actions[0]).toHaveProperty('type', 'SHOW_SEARCH');
+  expect(actions[0]).toHaveProperty('payload', false);
+});
+
+test('mock response', () => {
+  const response = {
+    status: 200,
+    data: {},
+  };
+  const store = mockStore({});
+  validateSearchTrackArtists(response, store.dispatch);
+
+  const actions = store.getActions();
+  expect(actions[0]).toHaveProperty('type', 'SPOTIFY_SEARCH');
+  expect(actions[0]).toHaveProperty('payload', response.data);
+});
+
+
+test('mock response with error', () => {
+  const response = {
+    status: 500,
+  };
+  const store = mockStore({});
+  validateSearchTrackArtists(response, store.dispatch);
+
+  const actions = store.getActions();
+  console.log('actions => ', actions);
+  expect(actions[0]).toHaveProperty('type', 'SPOTIFY_SEARCH_ERROR');
+  expect(actions[0]).toHaveProperty('payload', '');
+});
